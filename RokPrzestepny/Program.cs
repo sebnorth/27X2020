f4ba3bebcb31bfc34addb2be81a729b401788880
sebnorth <sebnorth@gmail.com>
2020-10-27 12:12:45 +0100

    Dodaj pliki projektów.

---
+++ RokPrzestepny/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RokPrzestepny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok: ");
+            int.TryParse(Console.ReadLine(), out int rok);
+
+            if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0))
+                Console.WriteLine("Rok {0} jest przestępny", rok);
+            else
+                Console.WriteLine("Rok {0} nie jest przestępny", rok);
+
+
+            Console.ReadKey();
+        }
+    }
+}
